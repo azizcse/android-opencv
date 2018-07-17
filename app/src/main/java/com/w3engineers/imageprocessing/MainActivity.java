a6b6f867b486a9295513a8c977173df00f2f51ed
Aziz <imazizul@gmail.com>
2018-07-17 06:20:25 +0600

    comment

--- app/src/main/java/com/w3engineers/imageprocessing/MainActivity.java
+++ app/src/main/java/com/w3engineers/imageprocessing/MainActivity.java
@@ -1,220 +1,229 @@
 package com.w3engineers.imageprocessing;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
-import android.os.AsyncTask;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.SurfaceView;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.googlecode.tesseract.android.TessBaseAPI;
-
 import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.CvType;
+import org.opencv.core.KeyPoint;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.features2d.FeatureDetector;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity /*implements CameraBridgeViewBase.CvCameraViewListener2*/ {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Uri outputFileDir;
+    private static final String DATA_PATH = Environment.getExternalStorageDirectory().toString()+"/pethoalpar";
 
-    private JavaCameraView javaCameraView;
-    private Mat mMat, imageGray, imageCanny;
-    private static final String DATA_PATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/com.w3engineers.imageprocessing/";
+    private ImageView imageView;
 
-    BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+    Mat imageMat;
+    Mat imageMat2;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
-            switch (status) {
-                case BaseLoaderCallback.SUCCESS:
-                    javaCameraView.enableView();
-                    break;
-                default:
+            switch (status){
+                case LoaderCallbackInterface.SUCCESS:{
+                    imageMat = new Mat();
+                    imageMat2 = new Mat();
+                }
+                break;
+                default:{
                     super.onManagerConnected(status);
-                    break;
+                }
+                break;
             }
-
         }
     };
-    ImageView imageView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.image_picker_view);
-
-        imageView = findViewById(R.id.imageView);
-        if (OpenCVLoader.initDebug()) {
-            Toast.makeText(this, "Opencv init success", Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(this, "Opencv init failed", Toast.LENGTH_LONG).show();
-        }
-        /*if(PermissionUtil.init(this).request(Manifest.permission.CAMERA)) {
-            javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
-            javaCameraView.setVisibility(SurfaceView.VISIBLE);
-
-            javaCameraView.setCvCameraViewListener(this);
-        }*/
-    }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    public void openGlarrey(View view){
-        if(PermissionUtil.init(this).request(Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            File file = new File(DATA_PATH);
-
-            if(!file.exists()){
-                file.mkdirs();
-            }
-            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(intent, 100);
-        }
-    }
-
-
-
-    public void convertGray(View view){
-        Mat rgb = new Mat();
-        Mat gray = new Mat();
-        BitmapFactory.Options options = new BitmapFactory.Options();
-
-        options.inDither = false;
-        options.inSampleSize = 4;
-
-        int width = imageBitmap.getWidth();
-        int hight = imageBitmap.getHeight();
-        grayBitmap = Bitmap.createBitmap(width, hight, Bitmap.Config.RGB_565);
-
-        Utils.bitmapToMat(imageBitmap, rgb);
-        Imgproc.cvtColor(rgb, gray, Imgproc.COLOR_RGB2GRAY);
-        Imgproc.Canny(gray, gray, 50,150);
-        Utils.matToBitmap(gray, grayBitmap);
-
-        imageView.setImageBitmap(grayBitmap);
-        new ocrTask(grayBitmap).execute();
-    }
-
-    Uri imageUri;
-    Bitmap grayBitmap, imageBitmap;
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == 100 && resultCode == RESULT_OK && data != null){
-            imageUri = data.getData();
-            try {
-                imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            imageView.setImageBitmap(imageBitmap);
-
-
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        /*if (javaCameraView != null) {
-            javaCameraView.disableView();
-        }*/
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        /*if (javaCameraView != null) {
-            javaCameraView.disableView();
-        }*/
-    }
 
     @Override
     protected void onResume() {
         super.onResume();
-        /*if (OpenCVLoader.initDebug()) {
-            mLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
-        } else {
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, mLoaderCallback);
-        }*/
-    }
-
-    /*@Override
-    public void onCameraViewStarted(int width, int height) {
-        mMat = new Mat(width, height, CvType.CV_8UC4);
-        imageGray = new Mat(width, height, CvType.CV_8UC1);
-        imageCanny = new Mat(width, height, CvType.CV_8UC1);
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG,"OpenCv problem");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
+        }else{
+            Log.d(TAG, "OpenCV initiated success");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
     }
 
     @Override
-    public void onCameraViewStopped() {
-        mMat.release();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.example_activity);
+        final Activity activity = this;
+        imageView = (ImageView) this.findViewById(R.id.imageView);
+        this.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},120);
+                }
+                if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},121);
+                }
+                startCameraActivity();
+            }
+        });
+    }
+
+    private void startCameraActivity(){
+        try{
+            String imagePath = DATA_PATH + "/imgs";
+            File dir = new File(imagePath);
+            if(!dir.exists()){
+                dir.mkdir();
+            }
+            String imageFilePath = imagePath + "/ocr.jpg";
+            outputFileDir = Uri.fromFile(new File(imageFilePath));
+            final Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileDir);
+            if( pictureIntent.resolveActivity(getPackageManager()) != null){
+                startActivityForResult(pictureIntent, 100);
+            }
+        } catch (Exception e){
+            Log.e(TAG, e.getMessage());
+        }
     }
 
     @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        mMat = inputFrame.rgba();
-        Imgproc.cvtColor(mMat, imageGray, Imgproc.COLOR_RGB2GRAY);
-        Imgproc.Canny(imageGray, imageCanny, 50,150);
-        return imageCanny;
-    }*/
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 100 && resultCode == Activity.RESULT_OK){
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 10;
+            Bitmap bitmap = BitmapFactory.decodeFile(outputFileDir.getPath(), options);
 
-
-    private class ocrTask extends AsyncTask<Void, Void, Void> {
-        Bitmap bitmap;
-        TessBaseAPI baseApi;
-        public ocrTask(Bitmap grayBitmap){
-            this.bitmap = grayBitmap;
-        }
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            Log.i("OCRTask","onPreExecute..");
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            Log.i("OCRTask","onPostExecute..");
-
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            Log.i("OCRTask","extracting..");
-            String text = recognizeText();
-            Log.i("OCRTask","extracting = "+text);
-            return null;
-        }
-
-        private String recognizeText(){
-            String language = "";
-
-            baseApi = new TessBaseAPI();
-            baseApi.init(DATA_PATH, "eng",TessBaseAPI.OEM_TESSERACT_ONLY);
-            baseApi.setImage(bitmap);
-            return baseApi.getUTF8Text();
-
+            ExifInterface ei = null;
+            try{
+                ei = new ExifInterface(outputFileDir.getPath());
+            } catch (IOException e){
+                Log.d(TAG,"IO problem");
+            }
+            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+            switch (orientation){
+                case ExifInterface.ORIENTATION_ROTATE_90 :
+                    bitmap = rotateImage(bitmap, 90);
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180 :
+                    bitmap = rotateImage(bitmap, 180);
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270 :
+                    bitmap = rotateImage(bitmap, 270);
+                    break;
+                case ExifInterface.ORIENTATION_NORMAL :
+                default:
+                    break;
+            }
+            Utils.bitmapToMat(bitmap, imageMat);
+            detectText(imageMat);
+            Bitmap newBitmap = bitmap.copy(bitmap.getConfig(),true);
+            Utils.matToBitmap(imageMat, newBitmap);
+            imageView.setImageBitmap(newBitmap);
+        }else{
+            Toast.makeText(getApplicationContext(),"Problem", Toast.LENGTH_SHORT).show();
         }
     }
 
+    private Bitmap rotateImage (Bitmap source, float angle){
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source,0 ,0, source.getWidth(), source.getHeight(), matrix, true);
+    }
 
+    private void detectText(Mat mat){
+        Imgproc.cvtColor(imageMat, imageMat2, Imgproc.COLOR_RGB2GRAY);
+        Mat mRgba = mat;
+        Mat mGray = imageMat2;
+
+        Scalar CONTOUR_COLOR = new Scalar(1, 255, 128, 0);
+        MatOfKeyPoint keyPoint = new MatOfKeyPoint();
+        List<KeyPoint> listPoint = new ArrayList<>();
+        KeyPoint kPoint = new KeyPoint();
+        Mat mask = Mat.zeros(mGray.size(), CvType.CV_8UC1);
+        int rectanx1;
+        int rectany1;
+        int rectanx2;
+        int rectany2;
+
+        Scalar zeros = new Scalar(0,0,0);
+        List<MatOfPoint> contour2 = new ArrayList<>();
+        Mat kernel = new Mat(1, 50, CvType.CV_8UC1, Scalar.all(255));
+        Mat morByte = new Mat();
+        Mat hierarchy = new Mat();
+
+        Rect rectan3 = new Rect();
+        int imgSize = mRgba.height() * mRgba.width();
+
+        if(true){
+            FeatureDetector detector = FeatureDetector.create(FeatureDetector.MSER);
+            detector.detect(mGray, keyPoint);
+            listPoint = keyPoint.toList();
+            for(int ind = 0; ind < listPoint.size(); ++ind){
+                kPoint = listPoint.get(ind);
+                rectanx1 = (int ) (kPoint.pt.x - 0.5 * kPoint.size);
+                rectany1 = (int ) (kPoint.pt.y - 0.5 * kPoint.size);
+
+                rectanx2 = (int) (kPoint.size);
+                rectany2 = (int) (kPoint.size);
+                if(rectanx1 <= 0){
+                    rectanx1 = 1;
+                }
+                if(rectany1 <= 0){
+                    rectany1 = 1;
+                }
+                if((rectanx1 + rectanx2) > mGray.width()){
+                    rectanx2 = mGray.width() - rectanx1;
+                }
+                if((rectany1 + rectany2) > mGray.height()){
+                    rectany2 = mGray.height() - rectany1;
+                }
+                Rect rectant = new Rect(rectanx1, rectany1, rectanx2, rectany2);
+                Mat roi = new Mat(mask, rectant);
+                roi.setTo(CONTOUR_COLOR);
+            }
+            Imgproc.morphologyEx(mask, morByte, Imgproc.MORPH_DILATE, kernel);
+            Imgproc.findContours(morByte, contour2, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+            for(int i = 0; i<contour2.size(); ++i){
+                rectan3 = Imgproc.boundingRect(contour2.get(i));
+                if(rectan3.area() > 0.5 * imgSize || rectan3.area()<100 || rectan3.width / rectan3.height < 2){
+                    Mat roi = new Mat(morByte, rectan3);
+                    roi.setTo(zeros);
+                }else{
+                    Imgproc.rectangle(mRgba, rectan3.br(), rectan3.tl(), CONTOUR_COLOR);
+                }
+            }
+        }
+    }
 }
+
